2cbf0056f1d534e12d0a1d0a7dca214c56e48165
manas8127 <contact2manas24@gmail.com>
2021-06-15 12:32:43 +0530

    accepted changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <p>Hello I am Manas</p>
-=======
       <p>Hello I am Manas Srivastava</p>
       <p> This is Ankita Kashyap </p>
->>>>>>> 06e278a3ee182e1df43f27cfa38638b4db20d461
     </div>
   );
 }
