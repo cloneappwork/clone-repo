06e278a3ee182e1df43f27cfa38638b4db20d461
Ankita-Kashyap <ankyap150@gmail.com>
2021-06-15 11:20:11 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>Hello I am Manas Srivastava</p>
-      <p>Yo! This is Ankita Kashyap </p>
+      <p> This is Ankita Kashyap </p>
     </div>
   );
 }
