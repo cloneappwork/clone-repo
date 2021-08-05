19b86739738d46645871615add91566cc92cea9b
Ankita-Kashyap <ankyap150@gmail.com>
2021-08-05 23:19:06 +0530

    25:37 comment section

--- src/containers/feed/index.js
+++ src/containers/feed/index.js
@@ -8,7 +8,10 @@ export default function Feed() {
         <div className = "feed">
             
             <Post profileUrl = "https://lh3.googleusercontent.com/a-/AOh14Ghjelj93LXeUr7zGIZBv3J9lggy61x3L9DblYU5qro=s96-c"
-            username ="Ankita"/>
+            username ="Ankita"
+            photoURl = "blob:http://localhost:3000/4945772e-9a95-462b-8c54-651b241e7823"
+            caption="HEY! This is my first post"
+            />
        
         </div>
     )
--- src/containers/post/index.js
+++ src/containers/post/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import "./style.css";
-export default function Post({profileUrl,username,id,photoUrl,caption,comments}) {
+export default function Post({profileUrl,username,id,photoURl,caption,comments}) {
    
     return (
         <div className = "post">
@@ -12,6 +12,28 @@ export default function Post({profileUrl,username,id,photoUrl,caption,comments})
                 <button
                 className ="post__delete">Delete</button>
             </div>
+
+            <div className ="post__center"> 
+                <img className ="post__photoUrl" src = {photoURl} />
+            </div>
+        <div>
+            <p>
+                <span style={{fontWeight : "500",
+                 marginRight:"4px"}} >
+                {username}
+                </span>
+                {caption}
+                </p>
+        </div>
+        {comments ? (
+             comments.map((comment) =>(
+            < comment username ={comment.username} 
+            caption ={comment.comment} />
+            ))
+        ) : (
+        <></> )}
+        
         </div>
     );
+        
 }
